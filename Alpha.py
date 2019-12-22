7435fff140a1d36cf844fc526554b1f49d8467df
Kyla Butler <kbutler30@bellsouth.net>
2019-12-22 14:11:39 +0000

    Alpha

---
+++ Alpha.py
@@ -0,0 +1,14 @@
+
+my_list=["A","B","C","D"]
+print (type(my_list))
+print (my_list)
+my_list .reverse()
+print (my_list)
+my_list .sort()
+print (my_list)
+my_list .pop(1)
+print (my_list)
+my_list .append("E")
+print (my_list)
+
+
