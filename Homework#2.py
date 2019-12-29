840c4c5b70a6a553c97dea6c5511eed10c0551c7
wedunsto <wedunsto@aggies.ncat.edu>
2019-12-29 02:08:17 -0500

    Added Homework #2

---
+++ Homework#2.py
@@ -0,0 +1,18 @@
+#This program contains 5 errors.
+#Please fix the errors, run the fixed code, and push a commented code to your GitHub
+#Due date: 01/02/20
+
+class StressRelief:
+def Intro_To_Tranquility(self):
+		print("There are many ways to achieve reduce stress.")
+	def Counting(self);
+		print("Counting your breaths can be a quick way to reduce stress")
+		for breaths in range(10):
+if breaths %2 == 0:
+				print("Breath in..."+str(breaths))
+				else:
+				print("Breath out..."+str(breaths))
+
+steps = StressRelief()
+steps.Intro_To_Tranquility
+steps.Counting()
