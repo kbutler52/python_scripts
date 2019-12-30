f2bbae468ae7d68854e09c109e726bc4a79ca00f
Kyla Butler <kbutler30@bellsouth.net>
2019-12-30 02:18:53 +0000

    Homework1_For.py

--- Homework1_If.py
+++ Homework1_If.py
@@ -1,7 +1,7 @@
 number = 5 # this our variable
 if number > 4:# This sets my condition
-	print("This number is greater than 4")
-elif number == 3:
-	print("This number is 3")
-else:
-	print("This number is less than 3")
+	print("This number is greater than 4")# this will print out if the previous condition is true
+elif number == 3: # this is our next condition to check against
+	print("This number is 3") # This will be printed out if the previous conditio is met
+else: # This will be our next condition if the other two are not met
+	print("This number is less than 3")# This will be printed out if the previous condtion is met
