b81d5d8581c716bd6d34369c49f4df352a3128ef
Kyla Butler <kbutler30@bellsouth.net>
2020-05-13 14:06:46 +0000

    hw1

---
+++ williamHw/homework1.py
@@ -0,0 +1,70 @@
+
+#05-13-2020- Homeowrk#1
+#Print the first way  is with the %
+
+#avocadoCount = 3
+# momsFood = " I have this many avocados:"
+# print(momsFood+ "%d" %(avocadoCount))
+
+#-------------------------------------------------------
+
+#avocadoCount = 3
+# momsFood = "I have this many avocados: "
+# print(momsFood + f"{avocadoCount}")
+
+#------------------------------------------------------
+# momsFood = "I have this many avocados: "
+#print( momsFood + "3")
+#---------------------------------------------------
+#Strawberries
+# strawberries= 2
+# momsFood = "I have this many strawberries:"
+# print(momsFood + "%d" %(strawberries))
+#---------------------------------------------------------
+# strawberries = 2
+# momsFood = "I have this many strawberries: "
+# print(momsFood + f" {strawberries}")
+#--------------------------------------------------------
+# momsFood = "I have this many strawberries: "
+# print( momsFood + "2")
+#----------------------------------------------------
+
+#Cantaloupe
+# cantaloupe = 1
+# momsFood = "I have this many cantaloupe: "
+# print(momsFood + f" {cantaloupe}")
+
+#------------------------------------------------------------------
+#momsFood = "I have this many Cantaloupe: "
+#print(momsFood + "1")
+
+#----------------------------------------------------------------
+
+#cantaloupe = 1
+#momsFood = " I have this many canataloupe:"
+# print(momsFood + "%d" %(cantaloupe))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
