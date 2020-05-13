3f7f0432d7814811457ff165067a08e8db0f7e8f
Kyla Butler <kbutler30@bellsouth.net>
2020-05-13 12:22:20 +0000

    momsfood

---
+++ williamHw/printmomFood.py
@@ -0,0 +1,61 @@
+
+#05-13-2020
+#This is homeowrk from lesson one with William Dunston
+#over Zoom lesson on how to printing 3 different ways
+#The first one will write is I have 3 Avocados
+
+#Print Stateemts: Display information to the console
+#Conditional Statements: Run  different commands depending on
+#conditions(S)
+#Looping Statements: Run command(s) continuously until a stop
+#condition is met.
+#print Statemetn: Display information to the Console
+
+#program from yesterdays lesson
+
+# momsFood = "Mom has this many avocados: "
+# print(momsFood + "3")
+
+#program #2 from yesterday's lesson- the below program will not work
+#becuase 3 is a  integer  and needs to be converted to a string
+#like the one above
+
+# momsFood= "Mom has this many avocados: "
+# print(momsFood+3)
+
+# Format the string
+#3 ways
+#%: %d-decimal;  %f-float; %s-string
+#.format()
+#f""
+# format the string- This program works
+
+# advocadoCount = 3
+# momsFood = "Mom has this many avocados: "
+# print(momsFood+ "%d" %(advocadoCount))
+
+
+#Format the string:the program works
+# avocadoCount = 3
+# momsFood = " Mom has this many avocados: "
+# print(momsFood+ "{}".format(avocadoCount))
+
+# print with f string this is the most powerful of them all
+
+# avocadoCount = 3
+# momsFood = "Mom has this many avocados: "
+# print(momsFood + f"{avocadoCount}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
