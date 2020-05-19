112a44847aaf3de75faace045d7ede53286ace77
Kyla Butler <kbutler30@bellsouth.net>
2020-05-19 20:30:28 +0000

    catmore

---
+++ williamHw/examplecond.py
@@ -0,0 +1,14 @@
+#05-19-2020
+#example of williams condition statement
+
+cats = 20
+dogs = 10
+#now we are going to do some fact checking
+
+print(cats <dogs) # checks the if statement
+print(not(cats <dogs))#checks the else statement
+
+if cats> dogs:
+    print("cats are more than dogs")
+else:
+    print("cats are less than dogs")
