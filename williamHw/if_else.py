dd27b18ec2338bceb05910d058c871cd0293074e
Kyla Butler <kbutler30@bellsouth.net>
2020-05-19 19:53:06 +0000

    checkcond

---
+++ williamHw/if_else.py
@@ -0,0 +1,14 @@
+
+#05-19-2020
+#if_else condition statements
+avocados =5
+strawberries = 5
+print(avocados > strawberries) # check the if condtion
+print(not(avocados > strawberries)) #check the else condition
+#the above part of this program is JUST fact checking!
+if avocados > strawberries:
+    print("We have more avocados than strawberries")
+else:
+    print("We have less avocados than strawberries")
+
+
