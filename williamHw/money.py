1e2a300e9150381cf8fae22de393dffc53a5d1df
Kyla Butler <kbutler30@bellsouth.net>
2020-05-19 20:45:13 +0000

    money

---
+++ williamHw/money.py
@@ -0,0 +1,11 @@
+#05-19-2020
+will_money = 50000
+kyla_money = 100000
+print(will_money < kyla_money)# chekcs the if statement
+print(not(will_money < kyla_money))#checks the else statement
+
+if will_money < kyla_money:
+    print("William has less money than Kyla")
+else:
+    print("William has more money that Kyla")
+
