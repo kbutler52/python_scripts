8ad12f0308b1f9484f54787a471b1a63e1f3baea
Kyla Butler <kbutler30@bellsouth.net>
2020-05-19 19:29:20 +0000

    Avocados

---
+++ williamHw/Avocados.py
@@ -0,0 +1,13 @@
+
+#05-19-2020
+#Lesson from William on condition statements
+
+Avocados = 5
+Strawberries = 5
+if(Avocados > Strawberries):
+    print("There are more Avocadoes than Strawberries")
+elif(Avocados < Strawberries ):
+    print("There are more Strawberries than Avocados")
+else:
+    print("Avocados it equal to Strawberries")
+
