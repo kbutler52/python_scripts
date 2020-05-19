70781449c9edc45f0b3d7c47b98d14cee8c614e6
Kyla Butler <kbutler30@bellsouth.net>
2020-05-19 21:17:40 +0000

    first2nd

---
+++ williamHw/first2n.py
@@ -0,0 +1,12 @@
+
+#05-19-2020
+first = 5
+second = 2
+print(first < second)#checks the if statement
+print(not(first<second))#checks the else statement
+
+if first < second:
+    print("5 is less than 2")
+else:
+    print("First and Second are not equal")
+
