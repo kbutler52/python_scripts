ff38b5b6a363fecc1746d05795a018ad9621ac9f
Kyla Butler <kbutler30@bellsouth.net>
2020-05-19 19:20:44 +0000

    conditionlesson

---
+++ williamHw/conditions.py
@@ -0,0 +1,31 @@
+#05-19-2020
+#"conparison that result in True or Fales.
+#(10 > 5)
+#10<5)
+#10 ==5)
+
+#IF statement
+#commands will only when the condition is true
+#when the condtion is false the command will be ignored
+
+#if(True):
+    #<Runs>
+#if(Fales):
+    #<doesnot run>
+
+# If else statement
+# Else: Looks at everything except previous condition(s)
+#If(False):
+#Else:
+#<Runs>
+#
+#If, Elif. Else Statements
+#works like i, its condition is only evaluated when if condition is false.
+#Better refines the else condition
+#if(False):
+#elif(False):
+#else:
+#    <Runs>
+
+
+
