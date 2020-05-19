7fee868494f37fd25836653b4e99901a77136fde
Kyla Butler <kbutler30@bellsouth.net>
2020-05-19 21:12:05 +0000

    2notgreater

---
+++ williamHw/hw2.py
@@ -0,0 +1,13 @@
+
+#05-19-2020
+#Williams HW#2
+first = 5
+second = 2
+print(first > second)#checks if statement
+print(not(first > second))# chekcs ekse statement
+if first > second:
+    print("5 is greater than 2")
+else:
+    print("5 is less than 2")
+
+
---
+++ williamHw/hw2b.py
@@ -0,0 +1,12 @@
+
+#05-19-2020
+first = 5
+second = 2
+print(first < second)#this checks the if statement
+print(not(first < second))#this checks the else statement
+
+if first>  second:
+    print("2 is not greater than 5")
+else:
+    print("2 is not greater than 5")
+
