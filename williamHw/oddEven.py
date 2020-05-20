ced441b7b5d731421b8374204c67c0e15e82fb1d
Kyla Butler <kbutler30@bellsouth.net>
2020-05-20 21:38:12 +0000

    oddEven

---
+++ williamHw/oddEven.py
@@ -0,0 +1,19 @@
+#05-20-2020
+#checking to see if number is odd
+
+cat = 5
+print(cat %2==0)#checks the if statement
+print(not(cat %2==0))#checks the else statement
+if cat %2 ==0:#checks to see if the dividen of 5/2 has a 0 remainder
+    print(f" {cat} is a even number")#if the above is true this will print
+else:#if the if statement is not true we come down here to check this condition
+    print(f"{cat} is a odd number")#if the above condition is true we print this statement
+
+
+
+
+
+
+
+
+
