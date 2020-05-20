7185211331a8d4a7791ec17adb1764b0a79fda1a
Kyla Butler <kbutler30@bellsouth.net>
2020-05-20 19:42:16 +0000

    avoc

---
+++ williamHw/loopWil.py
@@ -0,0 +1,12 @@
+#05-20-2020
+avocadoCount= 10#start condition
+
+while avocadoCount > 0:#stop the condition
+    print(f"Mom has {avocadoCount} avocadoCount")
+    avocadoCount = avocadoCount - 1
+
+
+
+
+
+
