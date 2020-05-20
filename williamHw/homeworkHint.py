957b6c0f74b139b63ca6542cc1e1a50d3c770d60
Kyla Butler <kbutler30@bellsouth.net>
2020-05-20 20:26:05 +0000

    homeHint

---
+++ williamHw/homeworkHint.py
@@ -0,0 +1,11 @@
+
+#05-20-2020
+#HW#3
+lemons = 1
+print (lemons %2 ==0)#divides lemons by 2, and sees if the remainder is 0 also checks our if condition
+print (not(lemons %2 ==0))#checks our else condition
+if lemons%2 ==0:
+    print(f"{lemons} is even")
+else:
+    print(f"{lemons} is odd")
+
