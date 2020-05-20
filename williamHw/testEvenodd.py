cd6a1fde7bf3f2cd6f5aff30da18ed9343933f6c
Kyla Butler <kbutler30@bellsouth.net>
2020-05-20 21:03:13 +0000

    testEvenodd

---
+++ williamHw/testEvenodd.py
@@ -0,0 +1,12 @@
+#05-20-2020
+# this program will check to see if the number is odd or even
+
+cat = 2
+print(cat % 2 == 0)#checks the if statement
+print(not(cat % 2 == 0))#check the else statement
+
+if cat % 2 ==0:
+    print(f" {cat} is even")
+else:
+    print(f"{cat} is odd")
+
