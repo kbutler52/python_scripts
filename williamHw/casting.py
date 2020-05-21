8d9e3c3a5223d5c527a028921f4eba19d66df1bd
Kyla Butler <kbutler30@bellsouth.net>
2020-05-21 19:23:39 +0000

    casting

---
+++ williamHw/casting.py
@@ -0,0 +1,13 @@
+#05-21-2020
+# cat = input("Enter: ")
+# print("I Love  "  + cat)
+
+#input will always store it's value as a string
+#that's why we did Enhanced input with casting
+
+#Casting- changes the data type of a value.
+#int(...): Whatever is inside, change to integer
+
+cat = int(input("Enter:  "))
+print(3 + cat)
+
