3047ab901862184f760c3f659d15130f4d3b6db1
Kyla Butler <kbutler30@bellsouth.net>
2020-05-21 21:05:11 +0000

    willLoop

---
+++ williamHw/willLoop.py
@@ -0,0 +1,87 @@
+
+#05-21-2020
+#Agenda
+#Go over start and stop conditions
+#Go over interations / Step
+#Go over while loops
+#Go over for loops
+#Start condition: Tells the loop what index or value to start at
+#Stop condition: Tells the loop what index or value to stop at
+#Iteration / Step: Change in the index or value as the loop runs
+
+#Example
+#0,1,2,3,4,5
+#Start condition: 0( Start at first element)
+#Stop condition:5(Stop at last element)
+#Iteration / Step: 1(visit every value)
+#0,1,2,3,4,5
+#Interation /Step: 2 (visit every OTHER elemt)
+#0,2,4
+#As long as the condition is true, the loop runs.
+#While loops are better for Variables( numbers)
+#While loop format:
+# While(true):
+#    <Run Code>
+# using a while loop 1st
+#start = 0#start condition
+#while start<=5:# this isour stop condition
+   #print(start)
+   #start = start + 1
+# another while Loop example- the one above is just Commented out
+#Example of the code before I type it out
+#avocadoCount = 10
+#while(avoacadoCount > 0):
+#print("Mom has this {} avocados". format(avocado))
+#avocadoCount = avocadoCount - 1
+
+avocadoCount = 10#Start condtion
+while(avocadoCount > 0):#stop Condition
+    print(f"Mom has  {avocadoCount}  Avocados")
+    avocadoCount = avocadoCount -1#Iteration / Step
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
