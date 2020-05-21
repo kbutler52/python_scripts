56ac3af65718d752b0d6521be7223b775a051a2b
Kyla Butler <kbutler30@bellsouth.net>
2020-05-21 21:31:52 +0000

    willloop

---
+++ williamHw/willForLoop.py
@@ -0,0 +1,25 @@
+#05-20-2020
+#for Loop
+#contains predefined start, stop, and iteration
+#Runs the loop until stop condition is met
+#Bettr for data structrues: List,sets,tuple, dictionaries...
+#Foramt of a For loop below
+#for <varialbe> in <predefined>:
+#<Run Code>
+#this for loop is safer that While loop
+#Example the program before I write it out
+
+#avocadoCount = 10
+#for days in range(avocadoCount, 0, -1)
+#print("Mom has this {} avocados". .format())
+#what is start? Stop? Step?
+#How does just the number print in each loop?
+
+#for loop example
+avocadoCount = 10#starting condition
+for days in range(avocadoCount, 0,-1):#because we are going down we have to use the step function
+    print(f"Mom has %d avocados" %(days))
+
+
+
+
