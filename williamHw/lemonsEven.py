335a5a8185e1a6f65090d7b512d075f37ec00b15
Kyla Butler <kbutler30@bellsouth.net>
2020-05-21 21:54:08 +0000

    lemons

---
+++ williamHw/lemonsEven.py
@@ -0,0 +1,10 @@
+#05-20-2020
+
+Lemons= 1
+print(Lemons %2 ==0) #divides lemons by 2 and sees if the remainder is 0 and checks our if condition
+print(not(Lemons %2 ==0))#This checks our else condition
+if Lemons %2 ==0:
+    print(f"{Lemons} is even")
+else:
+    print(f"{Lemons} is odd")
+
