5ef2e7c149c921ab2371651a676d6a84971817c7
Kyla Butler <kbutler30@bellsouth.net>
2020-05-22 14:49:29 +0000

    ifStatement

---
+++ williamHw/if_statement.py
@@ -0,0 +1,58 @@
+
+#05-22-2020
+#conditional statements
+#William's first lesson
+#Adenda
+#Go over Conditions
+#Review if Statements
+#Review if, else statements
+#Review if, elif, else statements
+#compariosn that result in True or Fales
+#(10 > 5)
+#(10< 5)
+#10 == 5)
+
+#print(10>5)
+#print(10 < 5)
+#print 10 == 5)
+#Example 1 Commented out
+#avocados = 5
+#strawberries = 5
+#if avocados .  strawberries:
+   #print("we have more avocados that strawberries")
+#print(avocados > strawberries)
+
+#Example 2 if/else
+#avocados = 10
+#strawberries = 5
+#print avocados > strawberries)
+#print(not(avocados > strawberries))
+
+#if avocados > strawberries:
+   # print("We have more avocados that strawberries")
+#else:
+   #("We have less avocados thatn strawberries")
+
+#Example 3 if/elif/else statement
+# works like if, its condition is only evaluated when the if condition is false
+# it better refines the esle condtion
+#if(False):
+# elif(False):
+#else:
+#<runs>
+
+
+avocados = 10
+strawberries = 10
+#Determine if you have more , equal to , orless avocados than strawberries.
+print(avocados > strawberries)
+print(avocados == strawberries)
+print(avocados < strawberries)
+
+if avocados > strawberries:
+    print("we have more avocados than strawberries")
+elif avocados ==strawberries:
+    print("We have equal amount of avocados and strawberries")
+else:
+    print("We have less avocados than strawberries")
+
