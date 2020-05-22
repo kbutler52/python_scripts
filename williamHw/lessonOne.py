fb4de12f2d5d13bea8698912245928b2ef58b5e1
Kyla Butler <kbutler30@bellsouth.net>
2020-05-22 13:50:16 +0000

    lessonone

---
+++ williamHw/lessonOne.py
@@ -0,0 +1,10 @@
+#05-22-2020
+# learing print formatting
+
+print(f" I have {3} Avocados") #this allow the number three to become a string# this is william's favorite
+print(" I have %d Strawberries" %(2))#the %d the % to take the number 2 and turn a string and place it in here.
+print("I have  {} canalope"  .format (1))# what is happening here is where the print statement see {}, it says whatevr #follows .format turn that into a string and place it here.
+
+#these are just three ways that we do print formats.
+
+
